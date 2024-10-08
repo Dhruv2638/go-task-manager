cd613bd722f410b7724c8eccfa205da4458c3129
Dhruv Nagar <ninja-dhruv@Dhruvs-MacBook-Air.local>
2024-10-08 17:56:44 +0530

    List all the command connected

--- cmd/list.go
+++ cmd/list.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"task-manager/db"
 
 	"github.com/spf13/cobra"
 )
@@ -10,7 +12,19 @@ var ListCmd = &cobra.Command{
 	Use:   "list",
 	Short: "Lists all of your tasks.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("first")
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("Something went wrong:", err.Error())
+			os.Exit(1)
+		}
+		if len(tasks) == 0 {
+			fmt.Println("You have no tasks to complete!")
+			return
+		}
+		fmt.Println("You have following tasks:")
+		for i, task := range tasks {
+			fmt.Printf("%d. %s\n", i+1, task.Value)
+		}
 	},
 }
 
